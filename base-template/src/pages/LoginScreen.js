5b38e5355e6b462a6f0e4ec2bbe13714a1ff6a79
unknown <lcapelotto@gmail.com>
2020-04-28 01:58:18 -0300

    1.2

--- base-template/src/pages/LoginScreen.js
+++ base-template/src/pages/LoginScreen.js
@@ -14,20 +14,26 @@ export default class HomeScreen extends Component {
 
   render() {
     return (
-      <Block flex={1} center  space="evenly">
-        <Image
-          style={{ width: 350, height: 140 }}
-          source={{ uri: 'https://storage.googleapis.com/atados-v3/user-uploaded/images/8010b1b6-32bc-4dd4-bc11-a4a607c9974c.png' }}
-        />
-        <Block flex={1} middle>
-        <Input placeholder="Informe o seu emaill" color={theme.COLORS.SUCCESS} style={{ borderColor: "#ff4d00"}} placeholderTextColor={theme.COLORS.INFO} />
-        <Input placeholder="Informe a sua senha" color={theme.COLORS.SUCCESS} style={{ borderColor: "#ff4d00" }} placeholderTextColor={theme.COLORS.INFO}  />
-        </Block>
-        <Block row flex={2} top  space="evenly">
-          <Button middle round size="small" color="#ff4d00" >Logar</Button>
-          <Button middle round size="small" color="#ff4d00">Cadastrar</Button>
-        </Block>
-      </Block>
+      <View flex={1} style={{ justifyContent: "space-evenly" }}>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            style={{ width: 350, height: 140 }}
+            source={{ uri: 'https://storage.googleapis.com/atados-v3/user-uploaded/images/8010b1b6-32bc-4dd4-bc11-a4a607c9974c.png' }}
+          />
+        </View>
+        <View>
+          <Input placeholder="Informe o seu emaill" color={theme.COLORS.SUCCESS} style={{ borderColor: "#ff4d00" }} placeholderTextColor={theme.COLORS.INFO} />
+          <Input placeholder="Informe a sua senha" color={theme.COLORS.SUCCESS} style={{ borderColor: "#ff4d00" }} placeholderTextColor={theme.COLORS.INFO} />
+        </View>
+        <View style={{ alignItems: "center" }}>
+
+          <View style={{ flexDirection:"row", alignItems:"center" }} >
+            <Button shadowless round size="small" color="#ff4d00" >Logar</Button>
+            <Button shadowless round size="small" color="#ff4d00">Cadastrar</Button>
+          </View>
+
+        </View>
+      </View>
     );
   }
 }
